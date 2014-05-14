a9d4a63432971a2d6bddbc88a167f2e781f9199e
Mongoose <chienyu.inbox@gmail.com>
2014-05-13 17:07:25 -0700

    first test ever

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Chien on 5/1/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AFNetworking/AFNetworking.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by An on 5/1/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
